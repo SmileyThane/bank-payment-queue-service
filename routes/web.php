a9ba1cbaad2594bccdaae08f003e3638ad13f6f3
Hleb Prakhnitski <thane.crios@gmail.com>
2025-01-23 16:24:51 +0400

    created routes

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Http\Controllers\ClientController;
+use App\Http\Controllers\PaymentController;
+use App\Http\Controllers\UploadController;
+
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/', [UploadController::class, 'index'])->name('home');
+    Route::post('/upload', [UploadController::class, 'uploadFile'])->name('upload');
+    Route::get('/upload/{hash}', [ClientController::class, 'showByHash'])->name('clients');
+    Route::get('/upload/{hash}/pay', [PaymentController::class, 'initPayment'])->name('initPayment');
+
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
