7c7a5e720fbc2933044db58ab3a45a6bae5b2883
Hleb Prakhnitski <thane.crios@gmail.com>
2025-01-23 16:23:56 +0400

    created components for views

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>{{ config('app.name', 'App') }}</title>
+    <!-- Include Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+<div class="container mt-4">
+    <div class="min-h-screen bg-gray-100">
+        @include('layouts.navigation')
+        @isset($header)
+            <header class="bg-white shadow">
+                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    {{ $header }}
+                </div>
+            </header>
+        @endisset
+        <main>
+            @yield('content')
+        </main>
+    </div>
+</div>
+<footer class="text-center mt-4">
+    <p>&copy; {{ date('Y') }} {{ config('app.name', 'App') }} Application</p>
+</footer>
+</body>
+</html>
