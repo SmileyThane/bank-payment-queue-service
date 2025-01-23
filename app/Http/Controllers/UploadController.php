a40a16452de2bf1c360c0de9b1f5a854530022d4
Hleb Prakhnitski <thane.crios@gmail.com>
2025-01-23 16:23:05 +0400

    created upload controller

---
+++ app/Http/Controllers/UploadController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use App\Models\Upload;
+use App\Models\Client;
+use Illuminate\Support\Str;
+use Illuminate\View\View;
+use Maatwebsite\Excel\Facades\Excel;
+
+class UploadController extends Controller
+{
+    final public function uploadFile(Request $request): RedirectResponse
+    {
+        $request->validate(['file' => 'required|file|mimes:xlsx']);
+
+        $file = $request->file('file');
+        $referenceId = $request->string('reference_id');
+        $fileName = $file->getClientOriginalName();
+        $filePath = $file->store('uploads');
+        $outstandingAmount = 0;
+
+        $hash = Str::uuid();
+        $data = Excel::toCollection(null, $file);
+        $upload = Upload::query()->create([
+            'file_name' => $fileName,
+            'file_path' => $filePath,
+            'hash' => $hash,
+            'reference_id' => $referenceId,
+        ]);
+
+        foreach ($data[0] as $key => $row) {
+            if ($key === 0) {
+                continue;
+            }
+
+            $outstandingAmount += $row[4];
+            Client::query()->create([
+                'name' => $row[0],
+                'surname' => $row[1],
+                'patronymic' => $row[2],
+                'card_number' => (string)$row[3],
+                'amount' => $row[4],
+                'upload_id' => $upload->id,
+            ]);
+        }
+
+        $upload->outstanding_amount = $outstandingAmount;
+        $upload->save();
+
+        return back()->with('success', 'File uploaded successfully. Hash: ' . $hash);
+    }
+
+    final public function index(): View
+    {
+        $uploads = Upload::all();
+        $balance = (new PaymentController())->getActualBalance();
+        return view('home', compact('balance', 'uploads'));
+    }
+}
+
