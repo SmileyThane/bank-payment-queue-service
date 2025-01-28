f8c4b0d7b7b7fb46e812f1f0f2945ec109fa4563
Hleb Prakhnitski <thane.crios@gmail.com>
2025-01-28 10:17:01 +0400

    created CRUD for beneficiaries improved payment methods

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -17,40 +17,6 @@ class PaymentController extends Controller
     /**
      * @throws JsonException
      */
-    final public function authorize(): void
-    {
-
-
-        $curl = curl_init();
-
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => 'https://api-gate.vestabankdev.ru/release/api/sso/connect/token',
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'POST',
-            CURLOPT_POSTFIELDS => 'client_id=' . env('BANK_CLIENT') . '&client_secret=' . env('BANK_SECRET') . '&scope=' . env('BANK_SCOPE') . '&grant_type=' . env('BANK_GRANT_TYPE'),
-            CURLOPT_HTTPHEADER => array(
-                'Content-Type: application/x-www-form-urlencoded'
-            ),
-        ));
-
-        $response = curl_exec($curl);
-        curl_close($curl);
-
-        $result = json_decode($response, true);
-
-        if ($result && $result['access_token']) {
-            Cache::add('access_token', $result['access_token'], 60);
-        }
-    }
-
-    /**
-     * @throws JsonException
-     */
     final public function getActualBalance(): float
     {
         if (!Cache::has('access_token')) {
@@ -88,12 +54,44 @@ class PaymentController extends Controller
     /**
      * @throws JsonException
      */
+    final public function authorize(): void
+    {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://api-gate.vestabankdev.ru/release/api/sso/connect/token',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => 'client_id=' . env('BANK_CLIENT') . '&client_secret=' . env('BANK_SECRET') . '&scope=' . env('BANK_SCOPE') . '&grant_type=' . env('BANK_GRANT_TYPE'),
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/x-www-form-urlencoded'
+            ),
+        ));
+
+        $response = curl_exec($curl);
+        curl_close($curl);
+
+        $result = json_decode($response, true);
+
+        if ($result && $result['access_token']) {
+            Cache::add('access_token', $result['access_token'], 60);
+        }
+    }
+
+    /**
+     * @throws JsonException
+     */
     final public function initPayment(Request $request, string $hash): RedirectResponse
     {
         $upload = Upload::query()->where('hash', $hash)->firstOrFail();
         if ($upload) {
-            $clients  = Client::query()->where('upload_id', $upload->id)->get();
-            CreatePaymentProcessJob::dispatch($clients, $request->payment_comment);
+            $clients = Client::query()->where('upload_id', $upload->id)->get();
+            CreatePaymentProcessJob::dispatch($clients, $upload->virtual_account_id, $request->payment_comment);
             $upload->is_executed = 1;
             $upload->save();
         }
@@ -101,11 +99,11 @@ class PaymentController extends Controller
         return redirect()->back();
     }
 
-    public function initPaymentProcess($clients, string $payment_comment):void
+    public function initPaymentProcess($clients, string $virtualAccountId, string $paymentComment): void
     {
         foreach ($clients as $client) {
-            $purpose = ( $payment_comment ?? 'Отправка на карту. ' ) . $client->name . ' ' . $client->surname;
-            $dealId = $this->createPayment($client->id, $client->card_number, $client->amount, $purpose);
+            $purpose = ($paymentComment ?? 'Отправка на карту. ') . $client->name . ' ' . $client->surname;
+            $dealId = $this->createPayment($client->id, $virtualAccountId, $client->card_number, $client->amount, $purpose);
             if ($dealId) {
                 $client->deal_id = $dealId;
                 $client->status = Client::STATUSES[1];
@@ -119,7 +117,7 @@ class PaymentController extends Controller
     /**
      * @throws JsonException
      */
-    private function createPayment(int $id, string $cardNumber, float $amount, string $purpose = ''):string|null
+    private function createPayment(int $id, string $virtualAccountId, string $cardNumber, float $amount, string $purpose = ''): string|null
     {
         if (!Cache::has('access_token')) {
             $this->authorize();
@@ -141,7 +139,7 @@ class PaymentController extends Controller
                                         "amount": ' . $amount . ',
                                         "payers": [
                                             {
-                                                "virtualAccountNumber": "6bd5adea-d1e1-4938-9ebe-d4962d50c160",
+                                                "virtualAccountNumber": "' . $virtualAccountId . '",
                                                 "amount": ' . $amount . '
                                             }
                                         ],
@@ -151,7 +149,7 @@ class PaymentController extends Controller
                                                 "type": "PaymentToCard",
                                                 "amount": ' . $amount . ',
                                                 "cardNumber": "' . $cardNumber . '",
-                                                "purpose": "' . $purpose. '",
+                                                "purpose": "' . $purpose . '",
                                                 "PaymentReceiverType": "SelfEmployed"
                                             }
                                         ]
@@ -178,10 +176,28 @@ class PaymentController extends Controller
         return null;
     }
 
+    private function setPaymentError(int $clientId, string $status, string $description = null, string $trace = null): void
+    {
+        $client = Client::query()->find($clientId);
+        if ($client) {
+            $client->status = $status;
+            if ($description) {
+                $client->status_description = $description;
+            } else {
+                $client->status_description = Client::PROBLEM_DEFAULT_MESSAGES[$status];
+                Log::error($trace);
+            }
+            $client->save();
+        }
+    }
+
     private function processPayment(int $clientId, string $dealId): void
     {
-        $curl = curl_init();
+        if (!Cache::has('access_token')) {
+            $this->authorize();
+        }
 
+        $curl = curl_init();
         curl_setopt_array($curl, array(
             CURLOPT_URL => 'https://api-gate.vestabankdev.ru/release/api/nominalaccounts-service/v2/partner/accounts/' . env('BANK_ACCOUNT_NUMBER') . '/deals/' . $dealId . '/execute',
             CURLOPT_RETURNTRANSFER => true,
@@ -213,25 +229,10 @@ class PaymentController extends Controller
         }
     }
 
-    private function setPaymentError(int $clientId, string $status, string $description = null, string $trace = null): void
-    {
-        $client = Client::query()->find($clientId);
-        if ($client) {
-            $client->status = $status;
-            if ($description) {
-                $client->status_description = $description;
-            } else {
-                $client->status_description = Client::PROBLEM_DEFAULT_MESSAGES[$status];
-                Log::error($trace);
-            }
-            $client->save();
-        }
-    }
-
     /**
      * @throws JsonException
      */
-    public function checkBulkPaymentStatuses():void
+    public function checkBulkPaymentStatuses(): void
     {
         $clients = Client::query()
             ->where('status', Client::STATUSES[3])
@@ -262,10 +263,9 @@ class PaymentController extends Controller
         }
     }
 
-    public function getDeal(string $dealId):array|null
+    public function getDeal(string $dealId): array|null
     {
         $curl = curl_init();
-
         curl_setopt_array($curl, array(
             CURLOPT_URL => 'https://api-gate.vestabankdev.ru/release/api/nominalaccounts-service/v2/partner/accounts/' . env('BANK_ACCOUNT_NUMBER') . '/deals/' . $dealId,
             CURLOPT_RETURNTRANSFER => true,
@@ -285,6 +285,159 @@ class PaymentController extends Controller
 
         if ($result && $result['isSuccess'] === true && $result['value']) {
             return $result['value'];
+        }
+
+        return null;
+    }
+
+    public function getVirtualAccountsList()
+    {
+        if (!Cache::has('access_token')) {
+            $this->authorize();
+        }
+
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://api-gate.vestabankdev.ru/release/api/nominalaccounts-service/v2/partner/accounts/40702810900110714822/virtual-accounts/search?perPage=100',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_HTTPHEADER => array(
+                'Authorization: Bearer ' . Cache::get('access_token')
+            ),
+        ));
+
+        $response = curl_exec($curl);
+        $result = json_decode($response, true);
+
+        if ($result && $result['isSuccess'] === true && isset($result['value']['virtualAccounts'])) {
+            return $result['value']['virtualAccounts'];
+        }
+
+        return [];
+    }
+
+    public function createBeneficiary(Request $request)
+    {
+
+        if (!Cache::has('access_token')) {
+            $this->authorize();
+        }
+
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://api-gate.vestabankdev.ru/release/api/nominalaccounts-service/v2/partner/accounts/' . env('BANK_ACCOUNT_NUMBER') . '/beneficiaries/createPhysicalAndVirtualAccount',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => '{
+                                        "inn": "' . $request->inn . '",
+                                        "accountType": "Standard",
+                                        "beneficiaryData": {
+                                            "birthDate": "' . $request->birth_date . '",
+                                            "birthPlace": "' . $request->birth_place . '",
+                                            "email": "' . $request->email . '",
+                                            "firstName": "' . $request->first_name . '",
+                                            "isResident": true,
+                                            "lastName": "' . $request->last_name . '",
+                                            "middleName": "' . $request->middle_name . '",
+                                            "passportDate": "' . $request->passport_date . '",
+                                            "passportIssuerCode": "' . $request->passport_issuer_code . '",
+                                            "passportIssuerName": "' . $request->passport_issuer_name . '",
+                                            "passportNumber": "' . $request->passport_number . '",
+                                            "passportSeries": "' . $request->passport_series . '",
+                                            "phoneNumber": "' . $request->phone_number . '",
+                                            "registrationAddress": "' . $request->address . '"
+                                        }
+                                    }',
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/json',
+                'Authorization: Bearer ' . Cache::get('access_token')
+            ),
+        ));
+
+        curl_close($curl);
+        $response = curl_exec($curl);
+        $result = json_decode($response, true);
+
+        if ($result && $result['isSuccess'] === true) {
+            return redirect()->route('upload');
+        }
+
+        return null;
+    }
+
+    public function getBeneficiary(string $beneficiaryId)
+    {
+        if (!Cache::has('access_token')) {
+            $this->authorize();
+        }
+
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://api-gate.vestabankdev.ru/release/api/nominalaccounts-service/v2/partner/accounts/' . env('BANK_ACCOUNT_NUMBER') . '/beneficiaries/' . $beneficiaryId,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_HTTPHEADER => array(
+                'Authorization: Bearer ' . Cache::get('access_token')
+            ),
+        ));
+
+        $response = curl_exec($curl);
+        $result = json_decode($response, true);
+
+        if ($result && $result['isSuccess'] === true && isset($result['value'])) {
+            return $result['value']['beneficiary'];
+        }
+
+        return [];
+    }
+
+    public function activateBeneficiary($beneficiaryId)
+    {
+
+        if (!Cache::has('access_token')) {
+            $this->authorize();
+        }
+
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://api-gate.vestabankdev.ru/release/api/nominalaccounts-service/v2/partner/accounts/' . env('BANK_ACCOUNT_NUMBER') . '/beneficiaries/' . $beneficiaryId . '/state',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => '{
+                                        "Value": true
+                                    }',
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/json',
+                'Authorization: Bearer ' . Cache::get('access_token')
+            ),
+        ));
+
+        curl_close($curl);
+        $response = curl_exec($curl);
+        $result = json_decode($response, true);
+
+        if ($result && $result['isSuccess'] === true) {
+            return redirect()->back();
         }
 
         return null;
