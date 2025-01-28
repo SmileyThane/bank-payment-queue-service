a0cdedeef6ded6220d1d0cba90cb815293ed025b
Hleb Prakhnitski <thane.crios@gmail.com>
2025-01-28 10:15:14 +0400

    updated Upload model

--- app/Models/Upload.php
+++ app/Models/Upload.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Upload extends Model
 {
-    protected $fillable = ['file_name', 'file_path', 'outstanding_amount', 'hash', 'reference_id'];
+    protected $fillable = ['file_name', 'file_path', 'outstanding_amount', 'hash', 'reference_id', 'beneficiary_id', 'virtual_account_id'];
 }
