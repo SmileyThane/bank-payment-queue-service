064e7d21950811929ac6c5bfc48f670e4e3cf43c
Hleb Prakhnitski <thane.crios@gmail.com>
2025-01-28 10:18:29 +0400

    added beneficiary info to clients page

--- resources/views/clients.blade.php
+++ resources/views/clients.blade.php
@@ -10,9 +10,32 @@
                 <h3 class="text-lg font-semibold text-blue-600">Клиенты</h3>
                 <div class="flex justify-between items-center mt-2">
                     <div>
-                        <p class="text-sm text-gray-600">Детали о загруженных клиентах отображены ниже</p>
+                        <div class="mb-3">
+                            @if(!$beneficiary['isActive'])
+                                <a href="{{ route('activateBeneficiary', ['beneficiaryId' => $beneficiary['id']]) }}"
+                                   class="bg-blue-600 text-white px-4 py-2 rounded-md text-sm font-medium hover:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-300">
+                                    <i class="bi bi-upload"></i>
+                                    Активировать
+                                </a>
+                            @endif
+                        </div>
+                        <p class="text-sm text-gray-600">Бенифициар активирован:
+                            <strong class="text-black">{{ $beneficiary['isActive'] ? 'Да' : 'Нет' }}</strong>
+                        </p>
+                        <p class="text-sm text-gray-600">ID бенифициара:
+                            <strong class="text-black">{{ $upload['beneficiary_id'] }}</strong>
+                        </p>
+                        <p class="text-sm text-gray-600">ФИО бенифициара:
+                            <strong class="text-black">{{ $beneficiary['data']['lastName'] }} {{ $beneficiary['data']['firstName'] }} {{ $beneficiary['data']['middleName'] }}</strong>
+                        </p>
+                        <p class="text-sm text-gray-600">Еmail бенифициара:
+                            <strong class="text-black">{{ $beneficiary['data']['email'] }}</strong>
+                        </p>
+
+                        <p class="text-sm text-gray-600">ID виртуального аккаунта: <strong
+                                    class="text-black">{{ $upload->virtual_account_id }}</strong></p>
                         <p class="text-sm text-gray-600">Актуальный баланс: <strong
-                                class="text-black">{{ $balance }}</strong></p>
+                                    class="text-black">{{ $balance }}</strong></p>
                     </div>
                     <div>
                         @if($isExecuted)
@@ -26,11 +49,12 @@
                                     <label for="payment_comment" class="block text-sm font-medium text-gray-700 mb-2">Комментарий</label>
                                     <input name="payment_comment" id="payment_comment"
                                            class="block w-full rounded-md border-gray-300 shadow-sm focus:ring-blue-500 focus:border-blue-500 text-sm"
-                                           required>
+                                    >
                                 </div>
                                 <button type="submit"
                                         class="bg-blue-600 text-white px-4 py-2 rounded-md text-sm font-medium hover:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-300">
-                                    <i class="bi bi-upload"></i> Оплатить
+                                    <i class="bi bi-upload"></i>
+                                    Оплатить
                                 </button>
                             </form>
                         @endif
@@ -61,19 +85,19 @@
                                 <td class="px-4 py-2 text-sm text-gray-700">{{ $client->patronymic }}</td>
                                 <td class="px-4 py-2">
                                     <span
-                                        class="inline-block bg-blue-100 text-blue-700 px-3 py-1 rounded-full text-xs font-medium shadow-sm">{{ $client->card_number }}</span>
+                                            class="inline-block bg-blue-100 text-blue-700 px-3 py-1 rounded-full text-xs font-medium shadow-sm">{{ $client->card_number }}</span>
                                 </td>
                                 <td class="px-4 py-2 text-sm text-gray-700 text-end">{{ $client->amount }}</td>
                                 <td class="px-4 py-2">
                                     <span
-                                        class="inline-block px-3 py-1 {{Client::STATUS_MESSAGES[$client->status]['color']}} rounded-full text-xs font-medium shadow-sm">
+                                            class="inline-block px-3 py-1 {{Client::STATUS_MESSAGES[$client->status]['color']}} rounded-full text-xs font-medium shadow-sm">
                                         {{ Client::STATUS_MESSAGES[$client->status]['text'] }}
                                     </span>
                                 </td>
                                 <td class="px-4 py-2">
                                     @if(!json_validate($client->status_description))
                                         <span
-                                            class="inline-block px-3 py-1 rounded-full text-xs font-medium shadow-sm bg-gray-100 text-gray-900">
+                                                class="inline-block px-3 py-1 rounded-full text-xs font-medium shadow-sm bg-gray-100 text-gray-900">
                                             {{ $client->status_description }}
                                         </span>
                                     @endif
