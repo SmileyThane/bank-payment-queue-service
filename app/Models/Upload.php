055fd9c0bd588833f3e50e983ff9f8ac662d1e62
Hleb Prakhnitski <thane.crios@gmail.com>
2025-03-18 14:00:01 +0300

    added soft deletes

--- app/Models/Upload.php
+++ app/Models/Upload.php
@@ -3,8 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Upload extends Model
 {
+    use SoftDeletes;
     protected $fillable = ['file_name', 'file_path', 'outstanding_amount', 'hash', 'reference_id', 'beneficiary_id', 'virtual_account_id'];
 }
