15898c1613a894ac1625f155bfd8eff5d97d8c44
Hleb Prakhnitski <thane.crios@gmail.com>
2025-03-19 14:01:35 +0300

    added calculations

--- app/Models/Upload.php
+++ app/Models/Upload.php
@@ -9,4 +9,12 @@ class Upload extends Model
 {
     use SoftDeletes;
     protected $fillable = ['file_name', 'file_path', 'outstanding_amount', 'hash', 'reference_id', 'beneficiary_id', 'virtual_account_id'];
+
+    protected $appends = ['clients_count', 'broken_clients_count'];
+    public function getClientsCountAttribute() {
+        return $this->hasMany(Client::class, 'upload_id')->count();
+    }
+    public function getBrokenClientsCountAttribute() {
+        return $this->hasMany(Client::class, 'upload_id')->whereIn('status', [Client::STATUSES[2], Client::STATUSES[6]])->count();
+    }
 }
