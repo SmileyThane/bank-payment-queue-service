7b0b41c0e651f0fb61e504c840b01b6e669d8c80
Hleb Prakhnitski <thane.crios@gmail.com>
2025-03-19 14:00:25 +0300

    created search

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Client;
 use App\Models\Upload;
+use Illuminate\Http\Request;
 use Illuminate\View\View;
 use JsonException;
 
@@ -12,7 +13,7 @@ class ClientController extends Controller
     /**
      * @throws JsonException
      */
-    final public function showByHash(string $hash): View
+    final public function showByHash(Request $request, string $hash): View
     {
         $upload = Upload::query()->where('hash', $hash)->firstOrFail();
         $id = $upload->id;
@@ -20,10 +21,34 @@ class ClientController extends Controller
         $isExecuted = $upload->is_executed;
         $isDealsCreated = $upload->is_deals_created;
         $isProcessed = $upload->is_processed;
-        $clients = Client::query()->where('upload_id', $id)->get();
+        $clientsQuery = Client::query()->where('upload_id', $id);
+        if ($search = $request->search) {
+            $clientsQuery->where(static function ($query) use ($search) {
+                $query->orWhere('name', 'like', '%' . $search . '%');
+                $query->orWhere('surname', 'like', '%' . $search . '%');
+                $query->orWhere('patronymic', 'like', '%' . $search . '%');
+                $query->orWhere('card_number', 'like', '%' . $search . '%');
+                $query->orWhere('amount', 'like', '%' . $search . '%');
+            });
+        }
+        $clients = $clientsQuery->get();
+        $processedClientsCount = Client::query()->where('upload_id', $id)->where('status', '=', Client::STATUSES[5])->count();
+
         $balance = (new PaymentController())->getVirtualActualBalance($upload->beneficiary_id, $upload->virtual_account_id);
         $beneficiary = (new PaymentController())->getBeneficiary($upload->beneficiary_id);
 
-        return view('clients', compact('clients', 'balance', 'hash', 'outstandingAmount', 'isExecuted', 'upload', 'beneficiary', 'isProcessed', 'isDealsCreated'));
+        return view('clients', compact(
+            'clients',
+            'balance',
+            'hash',
+            'outstandingAmount',
+            'isExecuted',
+            'upload',
+            'beneficiary',
+            'isProcessed',
+            'isDealsCreated',
+            'search',
+            'processedClientsCount',
+        ));
     }
 }
